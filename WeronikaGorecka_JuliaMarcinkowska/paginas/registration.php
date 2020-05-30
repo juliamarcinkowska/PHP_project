9764f0e6f05d60cf3790bf90b639ec2bf86314e3
Julia Marcinkowska <juliamar2205@gmail.com>
2020-05-30 19:00:14 +0100

    User already exists in the database

--- WeronikaGorecka_JuliaMarcinkowska/paginas/registration.php
+++ WeronikaGorecka_JuliaMarcinkowska/paginas/registration.php
@@ -9,6 +9,13 @@ if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["login"]) &&
     $login = $_POST["login"];
     $password = $_POST["password"];
     global $conn;
+    $sql_ch = "SELECT * FROM users WHERE login='" . $login . "'";
+    $result_ch = mysqli_query($conn, $sql_ch);
+    if (mysqli_num_rows($result_ch) != 0) {
+        echo "Registration failed, this login already exists, please select another one.";
+        header('refresh:2; url=./registration.html');
+        exit();
+    }
     $sql = "INSERT INTO users (name, email, login, password, usertype) VALUES (" . "'" . $name . "'" . "," . "'" . $email
         . "'" . "," . "'" . $login . "'" . "," . "'" . $password . "'" . "," . "1" . ")";
     $retval = mysqli_query($conn, $sql);
